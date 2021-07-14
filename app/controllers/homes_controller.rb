5122db3ff3068e71e80a5e817715c90711724087
EC2 Default User <ec2-user@ip-172-31-44-190.ap-northeast-1.compute.internal>
2021-07-14 13:08:23 +0000

    homesコントローラ

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @user = current_user.id
+   # @user = current_user.id
     @greats = Great.where(is_release: true)
     # @great_rank = @greats.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
     @great_rank = Great.find(Favorite.group(:great_id).order('count(great_id) desc').limit(3).pluck(:great_id))
