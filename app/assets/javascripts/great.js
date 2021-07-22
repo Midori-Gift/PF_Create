f8ae4e9d20461c8fb8b20bf7f279bceb1b78bedd
Midori-Gift <Mokometaberoyo5009@gmail.com>
2021-07-22 11:37:09 +0000

    favicon

--- app/assets/javascripts/great.js
+++ app/assets/javascripts/great.js
@@ -1,24 +1,23 @@
-<script>
-$(function() {
-  $(".search-input").on("keyup", function() {
-    let input = $(".search-input").val();
-    $.ajax({
-      type: 'GET',
-      url: '/greats/search',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    .done(function(greats) {
-      $(".contents.row").empty();
-      if (greats.length !== 0) {
-        greats.forEach(function(great){
-          appendGreat(great);
-        });
-      }
-      else {
-        appendErrMsgToHTML("一致する投稿がありません");
-      }
-    })
-  })
-})
-</script>
+
+// $(function() {
+//   $(".search-input").on("keyup", function() {
+//     let input = $(".search-input").val();
+//     $.ajax({
+//       type: 'GET',
+//       url: '/greats/search',
+//       data: { keyword: input },
+//       dataType: 'json'
+//     })
+//     .done(function(greats) {
+//       $(".contents.row").empty();
+//       if (greats.length !== 0) {
+//         greats.forEach(function(great){
+//           appendGreat(great);
+//         });
+//       }
+//       else {
+//         appendErrMsgToHTML("一致する投稿がありません");
+//       }
+//     })
+//   }
+// })
